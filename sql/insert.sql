6110c62d12f17a8b4311d2b1f10d156a41623b49
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2018-11-16 10:27:06 +0100

    Add files via upload

---
+++ sql/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO user_table (login, password, name, surname, adress, phone, admin) VALUES (
+	'riccardo_admin',
+	'riccardo_admin',
+	'Riccardo',
+	'Spolaor',
+	'Via x',
+	'444444444',
+	true
+);
+
+INSERT INTO user_table (login, password, name, surname, adress) VALUES (
+	'super_mario',
+	'mustache',
+	'Mario',
+	'Mario',
+	'Via dei Funghi 10, Regno dei Funghi'
+);
+
+INSERT INTO videogame (id, title, price, console, amount) VALUES (
+	1,
+	'God of War',
+	74.98,
+	'PS4',
+	30
+);
